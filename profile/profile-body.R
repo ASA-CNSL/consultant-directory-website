8fc712230b4c6c663ef6995062963bc3d0c7dcad
Neal Fultz <nfultz@gmail.com>
2026-02-27 20:37:43 +0000

    helper for optional profile fields

--- profile/profile-body.R
+++ profile/profile-body.R
@@ -1,6 +1,7 @@
 require(htmltools, quietly = TRUE)
 profile <- rmarkdown::metadata$profile
 
+optional <- function(nm, tag) if(hasName(profile, as.character(substitute(nm)))) tag
 
 div(
   h1(profile$name),
@@ -11,7 +12,10 @@ div(
     ),
     tags$li(
       profile$location
-    )
+    ),
+    optional(github, tags$li(
+      profile$github
+    ))
     
   )
 ) |> print()
