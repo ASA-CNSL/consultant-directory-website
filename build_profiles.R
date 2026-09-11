4077d5d406d13565ad4b2258c80b47dd429476a7
Ryan Peterson <ryan-peterson@uiowa.edu>
2026-09-11 12:18:32 -0500

    update with new responses, fix website issue on profile pages

--- build_profiles.R
+++ build_profiles.R
@@ -14,8 +14,12 @@ for (i in 1:nrow(form_responses)) {
   loc   <- row[["In-person availability (if applicable, list city/state/region)"]]
   loc <- ifelse(is.na(loc), "(not specified)", loc)
   web <- row[["Website URL (optional)"]]
-  web <- gsub("https://", "", web)
-  web <- paste0('https://', web) 
+  if(!is.na(web)) {
+    web <- gsub("https://", "", web)
+    web <- paste0('https://', web) 
+  } else 
+    web <- "not specified"
+  
   deg <- row[["Highest Degree"]]
   field <- ifelse(is.na(row[["Field of Degree"]]), "(field not specified)", row[["Field of Degree"]])
   context <- row[["Typical consulting context"]]
